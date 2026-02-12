a4c57ecd3854384e19f46c2d705019f99aff3dae
SrinadhKanupuru <kanupurusrinadh6@gmail.com>
2026-02-12 21:37:37 +0530

    adding routing

---
+++ my_app/src/Routing.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './Style.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './Header';
+import Hari from './Hari';
+import Subbu from './Subbu';
+import Srinadh from './Srinadh';
+
+const Routing = () => {
+  return (
+    <div>
+      <Header/>
+      <Routes>
+        <Route path='hari' element={<Hari/>}/>
+        <Route path='subbu' element={<Subbu/>}/>
+        <Route path='/' element={<Srinadh/>}/>
+        
+      </Routes>
+    </div>
+  )
+}
+
+export default Routing

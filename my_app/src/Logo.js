6bbd3262fab6e425d03903a44e988dc06e31493b
SrinadhKanupuru <kanupurusrinadh6@gmail.com>
2026-02-12 21:51:30 +0530

    update

---
+++ my_app/src/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='logo'>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbmDByzyZvqdiyqFd-80HoK6u9wCRHWL0aZg&s' alt='logo'/>      
+    </div>
+  )
+}
+
+export default Logo

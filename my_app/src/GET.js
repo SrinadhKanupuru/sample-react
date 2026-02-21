e702d6c234db120d2e4bf4c103776e85fd28950e
SrinadhKanupuru <kanupurusrinadh6@gmail.com>
2026-02-21 22:08:25 +0530

    api adding

--- my_app/src/GET.js
+++ my_app/src/GET.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import Axios from "axios"
+import "./Style.css"
 
 function GET() {
     const [name,setName]= useState([])
     useEffect(() => {
         Axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(res => {
-            console.log(res.data)
+            setName(res.data)
         })
         .catch(err =>{
             console.log(err)
@@ -16,11 +17,28 @@ function GET() {
   return (
     <div>
       <h1>Api printing</h1>
-      <table>
-        <tr> <th>name</th>
+      <table className='api'>
+        <thead className='id'>   
+        <tr> 
+        <th>userId</th>
         <th>id</th>
-        <th>number</th> </tr>
-       
+        <th>title</th> 
+        <th>body</th>
+        </tr>
+       </thead>
+       <tbody>
+        {name.map((am) => {
+         return(
+
+        <tr>
+          <td>{am.userId}</td>
+          <td>{am.id}</td>
+          <td>{am.title}</td>
+          <td>{am.body}</td>
+        </tr>
+         )  
+        })}
+       </tbody>
       </table>
     </div>
   )
